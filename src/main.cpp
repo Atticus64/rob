807ce7a936db25d31d26acbfb48d51825186072b
Jonathan <jonathanelian64@gmail.com>
2023-05-17 23:21:05 -0600

    feat: remove comments

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,6 @@ int main(int argc, char* argv[]) {
 
 	parse_args(argc, argv);
 
-	/* auto db = parse_csv("foo.csv"); */
-	/*  */
-	/* for(auto& item : db) { */
-	/*     std::cout << item.first << ' ' << item.second << std::endl; */
-	/*   } */
-	/*  */
-	/* db.erase("mid"); */
-	/* std::cout << "--------------------\n"; */
-	/*  */
-	/* for(auto& item : db) { */
-	/*     std::cout << item.first << ' ' << item.second << std::endl; */
-	/*   } */
-	/*  */
 	return 0;
 }
 
